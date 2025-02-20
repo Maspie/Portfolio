484c191b4923a3f1d2cf3780499e47c2dc0a6d01
Toshal <43895919+Maspie@users.noreply.github.com>
2025-02-20 07:43:13 -0700

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
-import { Hero } from "@/components/Hero"; // <== Use Hero instead
+import Home from "@/pages/Home";
 import { ThemeProvider } from "./lib/theme";
 
 function Router() {
